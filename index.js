a5a33c3c1c4eaf33b6bceed1eff947e285a3de5a
renatormsilva <renato.brtkm@gmail.com>
2021-04-28 21:30:59 -0400

    agoravai

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const socket = require("socket.io");
 const app = express();
 const cors = require("cors");
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const server = app.listen("5000", () => {
+const server = app.listen(PORT, () => {
   console.log("Server Running on Port 5000");
 });
 
